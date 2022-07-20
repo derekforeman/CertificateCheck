6de4b943b5e97f5bb14b5b7514ffeb2fc55c00a5
Derek <derekforeman@gmail.com>
2022-07-20 13:13:01 -0400

    ditto.

--- CertificateCheck.Api/Program.cs
+++ CertificateCheck.Api/Program.cs
@@ -21,25 +21,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.MapGet("/certcheck/{domain}", (CertService certService, string domain) =>
 {
     var deets =  certService.GetTest(domain);
@@ -54,8 +35,3 @@ app.MapGet("/certcheck/", ([FromBody] string[] domains) =>
 
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
